ae2618a75165f16a40e9096a870d7fd50550ae20
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-06-04 01:22:50 +0200

    fix: foo

--- src/get-package-config.js
+++ src/get-package-config.js
@@ -4,7 +4,7 @@ import fs from 'fs-extra';
 import { omit } from 'lodash-es';
 
 export default ({ cwd = '.' } = {}) => {
-  if (!fs.existsSync(pathLib.join(cwd, 'src', 'index.ts'))) {
+  if (!fs.existsSync(pathLib.join(cwd, 'src', 'index.ts')) && !fs.existsSync(pathLib.join(cwd, 'src', 'index.js'))) {
     return {};
   }
 
